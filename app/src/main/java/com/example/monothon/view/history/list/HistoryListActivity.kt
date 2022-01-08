455c19dddcc9e4b5039091ada0f5c4b1b5ca2704
이유승 <leeyouseng100@gmail.com>
2022-01-09 01:45:59 +0900

    이용 내역 리스트 셋

--- app/src/main/java/com/example/monothon/view/history/list/HistoryListActivity.kt
+++ app/src/main/java/com/example/monothon/view/history/list/HistoryListActivity.kt
@@ -1,6 +1,7 @@
 package com.example.monothon.view.history.list
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.monothon.R
@@ -10,16 +11,25 @@ class HistoryListActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityHistoryListBinding
 
+    val adapter = HistoryListAdapter {
+        Toast.makeText(this, "이용 내역 정보: $it", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history_list)
 
         initBinding()
+        initListView()
     }
 
     private fun initBinding() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_history_list)
         mBinding.lifecycleOwner = this
         mBinding.executePendingBindings()
+    }
+
+    private fun initListView() {
+        mBinding.recyclerView.adapter = adapter
     }
 }
