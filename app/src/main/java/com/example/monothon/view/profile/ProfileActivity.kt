4d8c1b0527ed02bd20362fcf7c00bce43edc2cca
kocenen <66261677+kocenen@users.noreply.github.com>
2022-01-09 04:09:13 +0900

    프로필

--- app/src/main/java/com/example/monothon/view/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/monothon/view/profile/ProfileActivity.kt
@@ -3,12 +3,16 @@ package com.example.monothon.view.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.monothon.R
+import com.example.monothon.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        ActivityProfileBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(binding.root)
 
     }
 }
